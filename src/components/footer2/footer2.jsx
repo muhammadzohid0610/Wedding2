e8e2e229aad59cf23483c25a31e362cea025a5e2
Muhammad <129236663+muhammadzohid0610@users.noreply.github.com>
2024-08-15 17:44:49 +0600

    asdasdasdasd

---
+++ src/components/footer2/footer2.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import './footer2.css'
+
+function Footer2(){
+    return(
+        <div class="footer_">
+			<h5>Sitemap</h5>
+			<div class="nav">
+				<a href="#">Home</a>
+				<a href="#">About</a>
+				<a href="#">Services</a>
+				<a href="#">Blog</a>
+				<a href="#">Contact Us</a>
+			</div>
+
+			<h1>
+				Thanks to the team
+			</h1>
+
+			<p class="creators">Photographer: <br/> Zhmen Aksionenko</p>
+			<p>Designer: Aslan Srazhidinov <br/>
+				Company: Prolab Academy <br/>
+				Instagram: prolab.academy <br/>
+				Gmail: prolabacademy@gmail.com
+			</p>
+
+			<p class="copy">
+				©COPYRIGHT 2024 PROLAB STUDIO <br/>
+				<span>
+					SITE DESIGN BY ASLAN
+				</span>
+			</p>
+		</div>
+    )
+}
+
+export default Footer2;
--- src/components/galary/galary.jsx
+++ src/components/galary/galary.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import Photo from '../photo/photo'
 import Galary from '../gallery/Gallery'
+import Footer from '../footer2/footer2'
 function GalleryPage() {
     return (
         <div>
            <Galary/>
 
            <Photo/>
+           <Footer/>
         </div>
     );
 }
